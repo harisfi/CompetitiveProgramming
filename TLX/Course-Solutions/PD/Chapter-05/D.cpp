9330c02f3de3be686c7c97b0a0c321ee3515b352
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-09-29 13:54:28 +0700

    Add solution
    Add solution for TLX>Course-Solutions>PD>Chapter-05

---
+++ TLX/Course-Solutions/PD/Chapter-05/D.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin>>n;
+	if(n>0)cout<<"positif"<<endl;
+	else if(n<0)cout<<"negatif"<<endl;
+	else cout<<"nol"<<endl;
+	return 0;
+}
