6a4bddb46f515b324e2c6d8818cf36bf4d68656d
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-10-09 13:22:58 +0700

    Add solution
    Add solution for TLX>Course-Solutions>PD>Chapter-11

---
+++ TLX/Course-Solutions/PD/Chapter-11/E.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	string s;
+	cin>>s;
+	for(int i=0;i<s.length();i++){
+		if(s[i]>=65&&s[i]<=90) s[i]+=32;
+		else s[i]-=32;
+	}
+	cout<<s<<endl;
+	return 0;
+}
