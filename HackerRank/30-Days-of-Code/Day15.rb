6637c12877da0d136a3aed0377979e06854049c9
Haris Firmansyah <acc.cp.haris@gmail.com>
2020-10-24 21:09:26 +0700

    Add solution
    Add solution for HackerRank>30-Days-of-Code

---
+++ HackerRank/30-Days-of-Code/Day15.rb
@@ -0,0 +1,42 @@
+class Node
+
+    attr_accessor :data,:next
+
+  def initialize data
+        @data = data
+        @next = nil
+    end
+end
+
+class Solution
+
+    def insert(head,value)
+        if head.nil? 
+            head = Node.new value
+        else
+            current = head
+            until current.next.nil? do
+                current = current.next
+            end
+            current.next = Node.new value
+        end
+        head
+    end
+
+	def display(head)
+       current = head
+       while current 
+          print current.data," "
+          current = current.next
+       end
+   end
+end
+       
+mylist= Solution.new
+head=nil
+T=gets.to_i
+for i in 1..T
+    data=gets.to_i
+    head=mylist.insert(head,data)
+end
+mylist.display(head)
